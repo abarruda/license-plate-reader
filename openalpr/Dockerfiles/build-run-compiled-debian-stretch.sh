d82d7305fc5bc38d817289e7c2563f874ee49b58
Aaron Arruda <aaron@abarruda.com>
2021-11-20 11:28:12 -0800

    Docker build and run updates

--- openalpr/Dockerfiles/build-run-compiled-debian-stretch.sh
+++ openalpr/Dockerfiles/build-run-compiled-debian-stretch.sh
@@ -4,9 +4,9 @@ set -e
 
 docker build -t openalpr:compiled-stretch-armv7 -f Dockerfile-compiled-debian-stretch --platform linux/arm/v7 ../.
 
-# docker run \
-#   --name openalpr \
-#   -it \
-#   --rm  \
-#   -v $(dirname `pwd`)/alprd.conf:/etc/openalpr/alprd.conf \
-#   openalpr:compiled-stretch-armv7
+docker run \
+  --name openalpr \
+  -it \
+  --rm  \
+  -v $(dirname `pwd`)/alprd.conf:/etc/openalpr/alprd.conf \
+  openalpr:compiled-stretch-armv7
